f120b84d1deb1ab89bf56733eb9cfcce008542b0
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-04-05 21:47:43 -0500

    Create index.js
    archivo principal

---
+++ TAREA1/index.js
@@ -0,0 +1,11 @@
+//funciones necesaria
+const {tomarplatosdecomida, tomarpacientes, tomarregistros} = require ('./fuctions')
+
+//muestra los platos de comida en base a la funcion
+tomarplatosdecomida();
+
+//muestra los pacientes de comida en base a la funcion
+tomarpacientes();
+
+//muestra los registros de comida en base a la funcion
+tomarregistros();

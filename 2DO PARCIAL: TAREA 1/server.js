907abaaa423d2a52fc40623e0ad7c70a8e85fb2b
luistuarezcastro <94723431+luistuarezcastro@users.noreply.github.com>
2023-06-09 10:32:42 -0500

    Add files via upload

---
+++ 2DO PARCIAL: TAREA 1/server.js
@@ -0,0 +1,56 @@
+const express =  require('express');
+const cors = require('cors');
+
+const { dbConnection } =  require('./database/config');
+
+
+class Server 
+{
+    constructor()
+    {
+        this.app = express.Router();
+        this.router = express.Router();
+
+        this.port = process.env.PORT;
+
+        //PATHS
+        this.paths = {
+            platos: '/api/platos',
+            pacientes: '/api/pacientes',
+            registros: '/api/registros',
+        }
+
+        this.connectDB();
+        this.middlewares();
+        this.routes();
+        this.router.use('/tarea6/calorias', this.app);
+        this._express = express().use(this.router);
+    }
+    async connectDB(){
+        await dbConnection();
+    }
+    middlewares(){
+        this.app.use(cors());
+        this.app.use(express.json())
+        this.app.use(express.static('public'));
+        this.app.use( '/uploads/', express.static('uploads'))
+
+    }
+    //CONFIGURACION DE RUTAS
+    routes(){
+        this.app.use(this.paths.platos, require('./routes/Plato')   )
+        this.app.use(this.paths.pacientes, require('./routes/Paciente')   )
+        this.app.use(this.paths.registros, require('./routes/Registro')   )
+        
+    }
+
+    listen(){
+        this._express.listen(this.port, ()=>{
+            console.log(`Servidor ejecuntando en puerto ${this.port}`)
+        })
+    }
+
+
+}
+
+module.exports = Server;
